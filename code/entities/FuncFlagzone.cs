68eb91405b38c2e52832daad0af97b80aae7cbfb
Matt Stevens <matt@mattstevens.co.uk>
2021-04-26 21:27:34 +0100

    Add func_flagzone trigger entity for flag capture

---
+++ code/entities/FuncFlagzone.cs
@@ -0,0 +1,52 @@
+﻿using Sandbox;
+
+namespace Fortwars
+{
+	[Library("func_flagzone")]
+	public partial class FuncFlagzone : ModelEntity
+	{
+		[HammerProp("team")]
+		public int Team { get; set; }
+
+		public override void Spawn()
+		{
+			base.Spawn();
+
+			SetupPhysicsFromModel(PhysicsMotionType.Static);
+			CollisionGroup = CollisionGroup.Trigger;
+			EnableSolidCollisions = false;
+			EnableTouch = true;
+
+			Transmit = TransmitType.Never;
+		}
+
+		public override void StartTouch(Entity other)
+		{
+			base.StartTouch(other);
+
+			if (other.IsWorld)
+				return;
+
+			if (other is Player)
+            {
+				var player = other as Player;
+				Log.Info($"{player.Name} StartTouch flag belonging to team {Team}");
+			}
+		}
+
+		public override void EndTouch(Entity other)
+		{
+			base.EndTouch(other);
+
+			if (other.IsWorld)
+				return;
+
+			if (other is Player)
+			{
+				var player = other as Player;
+				Log.Info($"{player.Name} EndTouch flag belonging to team {Team}");
+			}
+		}
+
+	}
+}
