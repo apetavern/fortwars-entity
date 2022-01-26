7f9ea66ff7fce2c5669080872a0c6645ae684036
Alex <alex@gu3.me>
2022-01-26 23:57:32 +0000

    Reduce camera leaning, make hud jiggle when moving player

--- code/player/Player.Camera.cs
+++ code/player/Player.Camera.cs
@@ -3,86 +3,97 @@ using System;
 
 namespace Fortwars
 {
-    partial class FortwarsPlayer
-    {
-        RealTimeSince timeSinceUpdatedFramerate;
+	partial class FortwarsPlayer
+	{
+		RealTimeSince timeSinceUpdatedFramerate;
 
-        Rotation lastCameraRot = Rotation.Identity;
+		Rotation lastCameraRot = Rotation.Identity;
 
-        public override void PostCameraSetup( ref CameraSetup setup )
-        {
-            base.PostCameraSetup( ref setup );
+		public override void PostCameraSetup( ref CameraSetup setup )
+		{
+			base.PostCameraSetup( ref setup );
 
-            if ( lastCameraRot == Rotation.Identity )
-                lastCameraRot = setup.Rotation;
+			if ( lastCameraRot == Rotation.Identity )
+				lastCameraRot = setup.Rotation;
 
-            var angleDiff = Rotation.Difference( lastCameraRot, setup.Rotation );
-            var angleDiffDegrees = angleDiff.Angle();
-            var allowance = 20.0f;
+			var angleDiff = Rotation.Difference( lastCameraRot, setup.Rotation );
+			var angleDiffDegrees = angleDiff.Angle();
+			var allowance = 20.0f;
 
-            if ( angleDiffDegrees > allowance )
-            {
-                // We could have a function that clamps a rotation to within x degrees of another rotation?
-                lastCameraRot = Rotation.Lerp( lastCameraRot, setup.Rotation, 1.0f - (allowance / angleDiffDegrees) );
-            }
-            else
-            {
-                //lastCameraRot = Rotation.Lerp( lastCameraRot, Camera.Rot, Time.Delta * 0.2f * angleDiffDegrees );
-            }
+			if ( angleDiffDegrees > allowance )
+			{
+				// We could have a function that clamps a rotation to within x degrees of another rotation?
+				lastCameraRot = Rotation.Lerp( lastCameraRot, setup.Rotation, 1.0f - (allowance / angleDiffDegrees) );
+			}
+			else
+			{
+				//lastCameraRot = Rotation.Lerp( lastCameraRot, Camera.Rot, Time.Delta * 0.2f * angleDiffDegrees );
+			}
 
-            // uncomment for lazy cam
-            //camera.Rot = lastCameraRot;
+			// uncomment for lazy cam
+			//camera.Rot = lastCameraRot;
 
-            if ( setup.Viewer != null )
-            {
-                AddCameraEffects( ref setup );
-            }
+			if ( setup.Viewer != null )
+			{
+				AddCameraEffects( ref setup );
+			}
 
-            if ( timeSinceUpdatedFramerate > 1 )
-            {
-                timeSinceUpdatedFramerate = 0;
-                //UpdateFps( (int) (1.0f / Time.Delta) );
-            }
-        }
+			if ( timeSinceUpdatedFramerate > 1 )
+			{
+				timeSinceUpdatedFramerate = 0;
+				//UpdateFps( (int) (1.0f / Time.Delta) );
+			}
+		}
 
-        float walkBob = 0;
-        float lean = 0;
-        float fov = 0;
+		float walkBob = 0;
+		float lean = 0;
+		float fov = 0;
 
-        private void AddCameraEffects( ref CameraSetup setup )
-        {
-            var speed = Velocity.Length.LerpInverse( 0, 320 );
-            var forwardspeed = Velocity.Normal.Dot( setup.Rotation.Forward );
+		private Vector3 lastCameraPos = Vector3.Zero;
+		private float lastHudOffset;
 
-            var left = setup.Rotation.Left;
-            var up = setup.Rotation.Up;
+		private void AddCameraEffects( ref CameraSetup setup )
+		{
+			var speed = Velocity.Length.LerpInverse( 0, 320 );
+			var forwardspeed = Velocity.Normal.Dot( setup.Rotation.Forward );
 
-            if ( GroundEntity != null )
-            {
-                walkBob += Time.Delta * 25.0f * speed;
-            }
+			var left = setup.Rotation.Left;
+			var up = setup.Rotation.Up;
 
-            setup.Position += up * MathF.Sin( walkBob ) * speed * 2;
-            setup.Position += left * MathF.Sin( walkBob * 0.6f ) * speed * 1;
+			if ( GroundEntity != null )
+			{
+				walkBob += Time.Delta * 25.0f * speed;
+			}
 
-            // Camera lean
-            lean = lean.LerpTo( Velocity.Dot( setup.Rotation.Right ) * 0.03f, Time.Delta * 15.0f );
+			setup.Position += up * MathF.Sin( walkBob ) * speed * 2;
+			setup.Position += left * MathF.Sin( walkBob * 0.6f ) * speed * 1;
 
-            var appliedLean = lean;
-            appliedLean += MathF.Sin( walkBob ) * speed * 0.2f;
-            setup.Rotation *= Rotation.From( 0, 0, appliedLean );
+			// Camera lean
+			lean = lean.LerpTo( Velocity.Dot( setup.Rotation.Right ) * 0.01f, Time.Delta * 15.0f );
 
-            speed = (speed - 0.7f).Clamp( 0, 1 ) * 3.0f;
+			var appliedLean = lean;
+			appliedLean += MathF.Sin( walkBob ) * speed * 0.2f;
+			setup.Rotation *= Rotation.From( 0, 0, appliedLean );
 
-            fov = fov.LerpTo( speed * 20 * MathF.Abs( forwardspeed ), Time.Delta * 2.0f );
+			speed = (speed - 0.7f).Clamp( 0, 1 ) * 3.0f;
 
-            setup.FieldOfView += fov;
+			fov = fov.LerpTo( speed * 20 * MathF.Abs( forwardspeed ), Time.Delta * 2.0f );
 
-            //	var tx = new Sandbox.UI.PanelTransform();
-            //	tx.AddRotation( 0, 0, lean * -0.1f );
+			setup.FieldOfView += fov;
 
-            //	Hud.CurrentPanel.Style.Transform = tx;
-            //	Hud.CurrentPanel.Style.Dirty();
-        }
-    }
+			var tx = new Sandbox.UI.PanelTransform();
+			tx.AddRotation( 0, 0, lean * -0.1f );
+
+			var zOffset = (lastCameraPos - setup.Position).z * 8f;
+			zOffset = zOffset.Clamp( -32, 32 );
+			zOffset = lastHudOffset.LerpTo( zOffset, 25.0f * Time.Delta );
+			tx.AddTranslateY( zOffset );
+
+			Local.Hud.Style.Transform = tx;
+			Local.Hud.Style.Dirty();
+
+			lastCameraPos = setup.Position;
+			lastHudOffset = zOffset;
+		}
+	}
 }
