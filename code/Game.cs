0fe05d07efd6f5b7e5e885093333ef1279c42350
Alex <alex@gu3.me>
2022-01-27 00:12:05 +0000

    Fix player spawns

--- code/Game.cs
+++ code/Game.cs
@@ -1,121 +1,140 @@
 using Sandbox;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Fortwars
 {
-    [Library( "fortwars", Title = "Fortwars" )]
-    partial class Game : Sandbox.Game
-    {
-        public static Game Instance
-        {
-            get => Current as Game;
-        }
+	[Library( "fortwars", Title = "Fortwars" )]
+	partial class Game : Sandbox.Game
+	{
+		public static Game Instance
+		{
+			get => Current as Game;
+		}
 
-        // shit hack, ideally Time.Now should be synced with server
-        [Net] public float ServerTime { get; private set; }
+		// shit hack, ideally Time.Now should be synced with server
+		[Net] public float ServerTime { get; private set; }
 
-        public Game()
-        {
-            if ( IsServer )
-                new UI.FortwarsHUD();
+		public Game()
+		{
+			if ( IsServer )
+				new UI.FortwarsHUD();
 
-            _ = StartTickTimer();
+			_ = StartTickTimer();
 
-            // todo: start in Lobby round
-            ChangeRound( new BuildRound() );
-        }
+			// todo: start in Lobby round
+			ChangeRound( new BuildRound() );
+		}
 
-        public async Task StartSecondTimer()
-        {
-            while ( true )
-            {
-                await Task.DelaySeconds( 1 );
-                OnSecond();
-            }
-        }
+		public async Task StartSecondTimer()
+		{
+			while ( true )
+			{
+				await Task.DelaySeconds( 1 );
+				OnSecond();
+			}
+		}
 
-        public async Task StartTickTimer()
-        {
-            while ( true )
-            {
-                await Task.NextPhysicsFrame();
-                OnTick();
-            }
-        }
+		public async Task StartTickTimer()
+		{
+			while ( true )
+			{
+				await Task.NextPhysicsFrame();
+				OnTick();
+			}
+		}
 
-        public override void PostLevelLoaded()
-        {
-            _ = StartSecondTimer();
+		public override void PostLevelLoaded()
+		{
+			_ = StartSecondTimer();
 
-            base.PostLevelLoaded();
-        }
+			base.PostLevelLoaded();
+		}
 
-        public override void ClientJoined( Client cl )
-        {
-            base.ClientJoined( cl );
+		public override void ClientJoined( Client cl )
+		{
+			base.ClientJoined( cl );
 
-            var player = new FortwarsPlayer();
-            player.Respawn();
+			var player = new FortwarsPlayer();
+			player.Respawn();
 
-            cl.Pawn = player;
-        }
+			cl.Pawn = player;
+		}
 
-        private void OnSecond()
-        {
-            // this is shite, need Time.Now to reflect server time clientside
-            if ( IsServer )
-            {
-                ServerTime = Time.Now;
-            }
+		private void OnSecond()
+		{
+			// this is shite, need Time.Now to reflect server time clientside
+			if ( IsServer )
+			{
+				ServerTime = Time.Now;
+			}
 
-            Round?.OnSecond();
-        }
+			Round?.OnSecond();
+		}
 
-        private void OnTick()
-        {
-            Round?.OnTick();
+		private void OnTick()
+		{
+			Round?.OnTick();
 
-            if ( IsClient )
-            {
-                // We have to hack around this for now until we can detect changes in net variables.
-                if ( _lastRound != Round )
-                {
-                    _lastRound?.Finish();
-                    _lastRound = Round;
-                    Round.Start();
-                }
-            }
-        }
+			if ( IsClient )
+			{
+				// We have to hack around this for now until we can detect changes in net variables.
+				if ( _lastRound != Round )
+				{
+					_lastRound?.Finish();
+					_lastRound = Round;
+					Round.Start();
+				}
+			}
+		}
 
-        [ServerCmd( "spawn" )]
-        public static void Spawn( string modelname )
-        {
-            var owner = ConsoleSystem.Caller;
-            var player = owner.Pawn;
+		public override void MoveToSpawnpoint( Entity pawn )
+		{
+			Log.Info( $"Finding spawnpoint for {pawn.Name} on TeamID: {(int)(pawn as FortwarsPlayer).TeamID}" );
 
-            if ( ConsoleSystem.Caller == null )
-                return;
+			var spawnpoints = Entity.All.OfType<InfoPlayerTeamspawn>().Where( x => x.Team == (int)(pawn as FortwarsPlayer).TeamID );
+			var randomSpawn = spawnpoints.OrderBy( x => Guid.NewGuid() ).FirstOrDefault();
 
-            var tr = Trace.Ray( player.EyePos, player.EyePos + player.EyeRot.Forward * 500 )
-                .UseHitboxes()
-                .Ignore( player )
-                .Size( 2 )
-                .Run();
+			if ( randomSpawn == null )
+			{
+				Log.Warning( "Couldn't find spawnpoint!" );
+				return;
+			}
 
-            var ent = new Prop();
-            ent.Position = tr.EndPos;
-            ent.Rotation = Rotation.From( new Angles( 0, player.EyeRot.Yaw(), 0 ) ) * Rotation.FromAxis( Vector3.Up, 180 );
-            ent.SetModel( modelname );
+			pawn.Position = randomSpawn.Position;
+			pawn.Rotation = randomSpawn.Rotation;
+		}
 
-            // Drop to floor
-            if ( ent.PhysicsBody != null && ent.PhysicsGroup.BodyCount == 1 )
-            {
-                var p = ent.PhysicsBody.FindClosestPoint( tr.EndPos );
+		[ServerCmd( "spawn" )]
+		public static void Spawn( string modelname )
+		{
+			var owner = ConsoleSystem.Caller;
+			var player = owner.Pawn;
 
-                var delta = p - tr.EndPos;
-                ent.PhysicsBody.Position -= delta;
-                //DebugOverlay.Line( p, tr.EndPos, 10, false );
-            }
-        }
-    }
+			if ( ConsoleSystem.Caller == null )
+				return;
+
+			var tr = Trace.Ray( player.EyePos, player.EyePos + player.EyeRot.Forward * 500 )
+				.UseHitboxes()
+				.Ignore( player )
+				.Size( 2 )
+				.Run();
+
+			var ent = new Prop();
+			ent.Position = tr.EndPos;
+			ent.Rotation = Rotation.From( new Angles( 0, player.EyeRot.Yaw(), 0 ) ) * Rotation.FromAxis( Vector3.Up, 180 );
+			ent.SetModel( modelname );
+
+			// Drop to floor
+			if ( ent.PhysicsBody != null && ent.PhysicsGroup.BodyCount == 1 )
+			{
+				var p = ent.PhysicsBody.FindClosestPoint( tr.EndPos );
+
+				var delta = p - tr.EndPos;
+				ent.PhysicsBody.Position -= delta;
+				//DebugOverlay.Line( p, tr.EndPos, 10, false );
+			}
+		}
+	}
 }
