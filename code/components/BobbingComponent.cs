7da65c9595d9850edfaebb3a8e6994cbc05b77db
Alex <alex@gu3.me>
2022-01-27 15:36:17 +0000

    Move bobbing logic over to a BobbingComponent

---
+++ code/components/BobbingComponent.cs
@@ -0,0 +1,30 @@
+﻿using Sandbox;
+using System;
+
+namespace Fortwars
+{
+	public partial class BobbingComponent : EntityComponent
+	{
+		[Net] public Vector3 CenterOffset { get; set; }
+
+		// TODO: move this to an entity component "BobComponent"
+		[Event.PreRender]
+		public virtual void OnPreRender()
+		{
+			if ( Entity is not ModelEntity { SceneObject: SceneObject sceneObject } )
+				return;
+
+			if ( sceneObject == null )
+				return;
+
+			sceneObject.Rotation = Rotation.From( 45, Time.Now * 90f, 0 );
+
+			// actual origin is off-center, let's just center that
+			Vector3 centerOffset = CenterOffset * sceneObject.Rotation;
+
+			// bob up and down
+			Vector3 bobbingOffset = Vector3.Up * MathF.Sin( Time.Now * 2f );
+			sceneObject.Position = Entity.Position + (centerOffset + bobbingOffset) * Entity.Scale;
+		}
+	}
+}
--- code/entities/FlagDisplay.cs
+++ code/entities/FlagDisplay.cs
@@ -19,22 +19,9 @@ namespace Fortwars
 
 			SetModel( "models/rust_props/small_junk/toilet_paper.vmdl" );
 			Scale = 5.0f;
-		}
 
-		[Event.PreRender]
-		public virtual void OnPreRender()
-		{
-			if ( this.SceneObject == null )
-				return;
-
-			SceneObject.Rotation = Rotation.From( 45, Time.Now * 90f, 0 );
-
-			// actual origin is off-center, let's just center that
-			Vector3 centerOffset = SceneObject.Rotation.Down * 3f;
-
-			// bob up and down
-			Vector3 bobbingOffset = Vector3.Up * MathF.Sin( Time.Now * 2f );
-			SceneObject.Position = Position + (centerOffset + bobbingOffset) * Scale;
+			var bobbingComponent = Components.Create<BobbingComponent>();
+			bobbingComponent.CenterOffset = Vector3.Down * 3f;
 		}
 	}
 }
