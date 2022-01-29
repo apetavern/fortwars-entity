4692f7b4184c119050a2ce291b622f406f7f7126
Alex <alex@gu3.me>
2022-01-29 18:03:02 +0000

    Move health bar worldpanel over to code/ui/world Display max health in health bar too

--- code/components/HealthBarComponent.cs
+++ code/components/HealthBarComponent.cs
@@ -62,35 +62,4 @@ namespace Fortwars
 			}
 		}
 	}
-
-	public class HealthBar : WorldPanel
-	{
-		private Label label;
-		private Panel inner;
-
-		private FortwarsBlock block;
-
-		internal HealthBar( FortwarsBlock block )
-		{
-			this.block = block;
-
-			StyleSheet.Load( "/components/HealthBar.scss" );
-			label = Add.Label( "0" );
-			inner = Add.Panel( "inner" );
-
-			SceneObject.ZBufferMode = ZBufferMode.None;
-			PanelBounds = new Rect( -500, -75, 1000, 150 );
-		}
-
-		public override void Tick()
-		{
-			base.Tick();
-
-			label.Text = block.Health.CeilToInt().ToString();
-			inner.Style.Width = Length.Fraction( block.Health / block.MaxHealth );
-
-			Scale = 2.0f;
-			WorldScale = 0.5f;
-		}
-	}
 }
---
+++ code/ui/world/HealthBar.cs
@@ -0,0 +1,37 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+namespace Fortwars
+{
+	public class HealthBar : WorldPanel
+	{
+		private Label label;
+		private Panel inner;
+
+		private FortwarsBlock block;
+
+		internal HealthBar( FortwarsBlock block )
+		{
+			this.block = block;
+
+			StyleSheet.Load( "/ui/world/HealthBar.scss" );
+			label = Add.Label( "0" );
+			inner = Add.Panel( "inner" );
+
+			SceneObject.ZBufferMode = ZBufferMode.None;
+			PanelBounds = new Rect( -500, -75, 1000, 150 );
+		}
+
+		public override void Tick()
+		{
+			base.Tick();
+
+			label.Text = $"{block.Health.CeilToInt()} / {block.MaxHealth.CeilToInt()}";
+			inner.Style.Width = Length.Fraction( block.Health / block.MaxHealth );
+
+			Scale = 2.0f;
+			WorldScale = 0.5f;
+		}
+	}
+}
