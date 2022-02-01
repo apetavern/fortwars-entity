3cc78f16737a6755c27b78bbc876bd44abface34
Alex <alex@gu3.me>
2022-02-01 14:46:38 +0000

    Min/max for viewmodel fov

--- code/weapons/ViewModel.cs
+++ code/weapons/ViewModel.cs
@@ -5,7 +5,7 @@ namespace Fortwars
 {
 	public class ViewModel : BaseViewModel
 	{
-		[ClientVar( "fw_viewmodel_fov", Help = "Viewmodel field of view" )]
+		[ClientVar( "fw_viewmodel_fov", Help = "Viewmodel field of view", Min = 50f, Max = 90f )]
 		public static float ViewmodelFov { get; set; } = 80f;
 
 		protected float SwingInfluence => 0.05f;
