8431d68b49012c862330250d9e0e600a336c6a2b
Alex <alex@gu3.me>
2022-02-01 02:07:45 +0000

    Experimental: shake the screen when heavy objects hit the ground

---
+++ code/components/GroundShakeComponent.cs
@@ -0,0 +1,68 @@
+﻿using Sandbox;
+using System.Linq;
+
+namespace Fortwars
+{
+	/// <summary>
+	/// Shake the ground when these objects hit something
+	/// </summary>
+	public class GroundShakeComponent : EntityComponent<FortwarsBlock>
+	{
+		TimeSince timeSinceLast;
+		bool wasIntersectingLastFrame;
+
+		[Event.Frame]
+		public void FrameUpdate()
+		{
+			var velocity = Entity.Velocity;
+			var tr = Trace.Sweep( Entity.PhysicsBody, Entity.Transform ).WorldOnly().Run();
+
+			if ( timeSinceLast < 0.2f )
+				return;
+
+			if ( tr.Hit && velocity.Length > 1 && !wasIntersectingLastFrame )
+			{
+				var shakeStrength = 512 - Vector3.DistanceBetween( CurrentView.Position, Entity.PhysicsBody.MassCenter );
+				shakeStrength /= 512f;
+				shakeStrength = shakeStrength.Clamp( 0, 1 );
+
+				shakeStrength *= velocity.Length;
+				shakeStrength *= Entity.PhysicsBody.Mass * 0.0025f;
+
+				shakeStrength *= 0.01f;
+				shakeStrength = shakeStrength.Clamp( 0, 10 );
+
+				Log.Trace( shakeStrength );
+
+				_ = new Sandbox.ScreenShake.Perlin( 0.5f, 1f, shakeStrength );
+				timeSinceLast = 0;
+				wasIntersectingLastFrame = true;
+			}
+			else
+			{
+				wasIntersectingLastFrame = false;
+			}
+		}
+
+		[Event.Frame]
+		public static void SystemUpdate()
+		{
+			foreach ( var entity in Sandbox.Entity.All.OfType<FortwarsBlock>() )
+			{
+				void Remove()
+				{
+					var existingGroundShake = entity.Components.Get<GroundShakeComponent>();
+					existingGroundShake?.Remove();
+				}
+
+				if ( !entity.IsValid() )
+				{
+					Remove();
+					continue;
+				}
+
+				entity.Components.GetOrCreate<GroundShakeComponent>();
+			}
+		}
+	}
+}
--- code/components/HealthBarComponent.cs
+++ code/components/HealthBarComponent.cs
@@ -1,6 +1,4 @@
 ﻿using Sandbox;
-using Sandbox.UI;
-using Sandbox.UI.Construct;
 using System.Linq;
 
 namespace Fortwars
