270f74e871dce81a28fe4a539caeca4f11cb6853
Alex <alex@gu3.me>
2022-02-03 01:10:55 +0000

    Initial work on a Rust-style pie menu

--- code/ui/BuildMenu.cs
+++ code/ui/BuildMenu.cs
@@ -1,5 +1,6 @@
 ﻿using Sandbox;
 using Sandbox.UI;
+using Sandbox.UI.Construct;
 using System;
 
 [Library]
@@ -7,13 +8,26 @@ public partial class BuildMenu : Panel
 {
 	public static BuildMenu Instance;
 
+	private Panel selection;
+	private Panel wrapper;
+
 	public BuildMenu()
 	{
 		Instance = this;
 
+		wrapper = Add.Panel( "wrapper" );
+
 		StyleSheet.Load( "/ui/BuildMenu.scss" );
 
-		var inner = Add.Panel( "inner" );
+		selection = wrapper.Add.Panel( "selected" );
+
+		var center = wrapper.Add.Panel( "center" );
+		center.Add.Icon( "question_mark", "image" );
+		center.Add.Label( "Name", "subtitle" );
+		center.Add.Label( "Lorem ipsum dolor sit amet", "description" );
+
+
+		var inner = wrapper.Add.Panel( "inner" );
 		var files = new string[] {
 			"fw_3x2.vmdl",
 			"fw_1x2.vmdl",
@@ -43,5 +57,33 @@ public partial class BuildMenu : Panel
 		}
 
 		BindClass( "active", () => Input.Down( InputButton.Menu ) );
+	}
+
+	float targetAngle = 0f;
+
+	public override void Tick()
+	{
+		base.Tick();
+
+		if ( IsVisible )
+		{
+			Vector2 relativeMousePos = Mouse.Position - wrapper.Box.Rect.Center;
+			float ang = MathF.Atan2( relativeMousePos.y, relativeMousePos.x )
+				.RadianToDegree().NormalizeDegrees();
+
+			ang = ang.SnapToGrid( 72f ) + 35f + 70f;
+			ang = ang.NormalizeDegrees();
+
+			float delta = targetAngle - ang;
+
+			if ( MathF.Abs( delta ) > 0.5f )
+			{
+				targetAngle = ang.NormalizeDegrees();
+
+				var tx = new PanelTransform();
+				tx.AddRotation( 0, 0, targetAngle );
+				selection.Style.Transform = tx;
+			}
+		}
 	}
 }
