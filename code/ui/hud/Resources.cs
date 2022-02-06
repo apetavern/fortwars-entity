67321b9d1bbad326855a01a663dd641621ad325d
Alex <alex@gu3.me>
2022-02-06 15:15:45 +0000

    Initial resources panel

---
+++ code/ui/hud/Resources.cs
@@ -0,0 +1,29 @@
+﻿using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+namespace Fortwars
+{
+	public class Resources : Panel
+	{
+		public Resources()
+		{
+			var wood = new Resource( "wood" );
+			wood.Parent = this;
+		}
+
+		class Resource : Panel
+		{
+			string name;
+
+			public Resource( string name )
+			{
+				this.name = name;
+
+				Add.Label( name, "name" );
+				Add.Label( "20", "count" );
+				Add.Label( "/", "separator" );
+				Add.Label( "20", "max" );
+			}
+		}
+	}
+}
