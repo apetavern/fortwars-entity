4908895d1c7682e9ca27c65cceb17e43f7b296c9
Alex <alex@gu3.me>
2022-02-08 14:01:13 +0000

    Chromatic aberration post-processing

--- code/player/Player.PostProcess.cs
+++ code/player/Player.PostProcess.cs
@@ -32,13 +32,8 @@ namespace Fortwars
 			postProcess.LensDistortion.K1 = 0f;
 			postProcess.LensDistortion.K2 = -0.005f;
 
-			//postProcess.ChromaticAberration.Enabled = true;
-			//postProcess.ChromaticAberration.Offset = Vector3.Up * 0.0005f;
-
-			//postProcess.Vignette.Enabled = true;
-			//postProcess.Vignette.Intensity = 0.5f;
-			//postProcess.Vignette.Roundness = 1.0f;
-			//postProcess.Vignette.Smoothness = 0.5f;
+			postProcess.ChromaticAberration.Enabled = true;
+			postProcess.ChromaticAberration.Offset = Vector3.Up * 0.0005f;
 		}
 	}
 }
