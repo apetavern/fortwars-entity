2ee3df8386515bd7a7b6bfe4da48ced0d9ef57c8
Alex <alex@gu3.me>
2022-02-10 02:29:37 +0000

    Initial custom crosshair styling

---
+++ code/ui/elements/crosshair/Crosshair.Config.cs
@@ -0,0 +1,46 @@
+﻿namespace Fortwars
+{
+	partial class Crosshair
+	{
+		public struct CrosshairConfig
+		{
+			public enum CrosshairStyle
+			{
+				Static,
+				Dynamic,
+				Dot
+			};
+
+			public CrosshairStyle Style { get; set; }
+
+			//
+			// Scaling
+			//
+			public int Size { get; set; }
+			public int Gap { get; set; }
+			public bool Outline { get; set; }
+			public int Thickness { get; set; }
+
+			//
+			// Color / opacity
+			//
+			public float Opacity { get; set; }
+			public Color Color { get; set; }
+			public Color OutlineColor { get; set; }
+
+			public static CrosshairConfig Default => new CrosshairConfig()
+			{
+				Style = CrosshairStyle.Dynamic,
+				Size = 64,
+				Gap = 16,
+
+				Thickness = 4,
+				Opacity = 1.0f,
+				Color = new Color( 1, 0, 1 ),
+
+				Outline = true,
+				OutlineColor = Color.Black
+			};
+		}
+	}
+}
---
+++ code/ui/elements/crosshair/Crosshair.cs
@@ -0,0 +1,108 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+namespace Fortwars;
+
+public partial class Crosshair : Panel
+{
+	private static Crosshair Instance { get; set; }
+
+	private static CrosshairConfig _config = CrosshairConfig.Default;
+	public static CrosshairConfig Config
+	{
+		get => _config; set
+		{
+			_config = value;
+			Instance.Build();
+		}
+	}
+
+	public Crosshair()
+	{
+		StyleSheet.Load( "/ui/elements/crosshair/Crosshair.scss" );
+
+		for ( int i = 0; i < 5; i++ )
+		{
+			var p = Add.Panel( "element" );
+			p.AddClass( $"el{i}" );
+		}
+
+		Instance = this;
+		Build();
+
+		BindClass( "visible", () =>
+		{
+			if ( VirtualCursor.InUse )
+				return false;
+
+			return Local.Pawn.ActiveChild is not FortwarsWeapon { IsAiming: true };
+		} );
+	}
+
+	/// <summary>
+	/// Builds the styling for the selected crosshair configuration
+	/// </summary>
+	private void Build()
+	{
+		StyleSheet.Parse( "crosshair.fortwarsweapon {" +
+
+			".el0, .el1, .el2, .el3 {" +
+			$"background-color: {Config.Color.Hex};" +
+			$"border: {(Config.Outline ? $"1px solid {Config.OutlineColor}" : "none")};" +
+			"}" +
+
+			//
+			// Left/right
+			//
+			".el0, .el1 " +
+			"{" +
+			$"width: {Config.Size}px;" +
+			$"height: {Config.Thickness}px;" +
+			"}" +
+
+			//
+			// Top/bottom
+			//
+			".el2, .el3 {" +
+			$"width: {Config.Thickness}px;" +
+			$"height: {Config.Size}px;" +
+			"}" +
+
+			".el0 {" +
+			$"right: {Config.Size}px;" +
+			"}" +
+
+			".el1 {" +
+			$"left: {Config.Size}px;" +
+			"}" +
+
+			".el2 {" +
+			$"bottom: {Config.Size}px;" +
+			"}" +
+
+			".el3 {" +
+			$"top: {Config.Size}px;" +
+			"}" +
+
+		"}" );
+	}
+
+	public override void Tick()
+	{
+		base.Tick();
+
+		if ( Local.Pawn.ActiveChild is not FortwarsWeapon weapon )
+			return;
+
+		float size = Config.Gap + (Config.Size * 2) + weapon.GetCrosshairSize();
+		Style.Width = size;
+		Style.Height = size;
+
+		// Vector2 screenScale = new Vector2( 1920, 1080 ) / Screen.Size;
+
+		var transform = new PanelTransform();
+		transform.AddScale( ScaleFromScreen );
+		transform.AddTranslate( Length.Percent( -50 ), Length.Percent( -50 ) );
+		Style.Transform = transform;
+	}
+}
