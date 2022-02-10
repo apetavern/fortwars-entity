c492347651d7d8399f9dbd62f18278f7ce269b72
Alex <alex@gu3.me>
2022-02-10 15:33:19 +0000

    Class menu styling

---
+++ code/ui/menus/ClassMenu.Preview.cs
@@ -0,0 +1,68 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+using System.Linq;
+
+namespace Fortwars
+{
+	partial class ClassMenu
+	{
+		public class ClassPreviewPanel : Panel
+		{
+			private ScenePanel renderScene;
+
+			private Rotation CameraRot => Rotation.From( 0, 210, 0 );
+			private Vector3 CameraPos => new Vector3( 70, 40, 48 );
+
+			AnimSceneObject citizen;
+
+			public ClassPreviewPanel()
+			{
+				Build();
+			}
+
+			public override void OnHotloaded()
+			{
+				base.OnHotloaded();
+
+				Build();
+			}
+
+			public override void Tick()
+			{
+				base.Tick();
+				Animate();
+			}
+
+			public void Animate()
+			{
+				citizen.Update( Time.Delta );
+				citizen.SetAnimBool( "grounded", true );
+				citizen.SetAnimFloat( "incline", 0f );
+			}
+
+			public void Build()
+			{
+				renderScene?.Delete( true );
+
+				Log.Trace( "Building render scene" );
+
+				using ( SceneWorld.SetCurrent( new SceneWorld() ) )
+				{
+					citizen = new AnimSceneObject( Model.Load( "models/citizen/citizen.vmdl" ), Transform.Zero );
+
+					Light.Point( Vector3.Up * 150.0f, 200.0f, Color.White * 5.0f );
+					Light.Point( Vector3.Up * 75.0f + Vector3.Forward * 100.0f, 200, Color.White * 15.0f );
+					Light.Point( Vector3.Up * 75.0f + Vector3.Backward * 100.0f, 200, Color.White * 15f );
+					Light.Point( Vector3.Up * 75.0f + Vector3.Right * 100.0f, 200, Color.White * 15.0f );
+					Light.Point( Vector3.Up * 100.0f + Vector3.Up, 200, Color.White * 15.0f );
+
+					renderScene = Add.ScenePanel( SceneWorld.Current, CameraPos, CameraRot, 75 );
+					renderScene.Style.Width = Length.Percent( 100 );
+					renderScene.Style.Height = Length.Percent( 100 );
+					renderScene.AmbientColor = new Color( .25f, .15f, .15f ) * 2.0f;
+				}
+			}
+		}
+	}
+}
--- code/ui/menus/ClassMenu.cs
+++ code/ui/menus/ClassMenu.cs
@@ -3,18 +3,20 @@ using Sandbox.UI.Construct;
 
 namespace Fortwars
 {
-	public class ClassMenu : Menu
+	public partial class ClassMenu : Menu
 	{
+		ClassInfo classInfo;
 		public ClassMenu()
 		{
 			StyleSheet.Load( "ui/menus/ClassMenu.scss" );
 			AddClass( "menu" );
 
 			Add.Label( "Select a class", "title" );
-			Add.Label( "Lorem ipsum dolor sit amet", "subtitle" );
+
+			var main = Add.Panel( "main" );
+			main.AddChild<ClassPreviewPanel>();
 
 			var classes = Add.Panel( "classes" );
-
 			var classArray = new Class[]
 			{
 				new AssaultClass(),
@@ -23,10 +25,38 @@ namespace Fortwars
 				new EngineerClass()
 			};
 
+			classInfo = AddChild<ClassInfo>();
+
 			foreach ( var classType in classArray )
 			{
-				var classButton = classes.Add.ButtonWithIcon( classType.Name, classType.IconPath, "class", () => Delete() );
+				var classButton = classes.Add.Button( classType.Name, "class", () => Delete() );
+				classButton.Add.Image( "ui/icons/placeholder.png", "class-icon" );
 				classButton.Add.Label( "0 / 0", "class-count" );
+
+				classButton.AddEventListener( "onmouseover", () =>
+				{
+					classInfo.Update( classType );
+				} );
+			}
+		}
+
+		class ClassInfo : Panel
+		{
+			Label name;
+			Label description;
+			Label loadout;
+
+			public ClassInfo()
+			{
+				name = Add.Label( "Class Name", "subtitle" );
+				description = Add.Label( "Class Description" );
+				loadout = Add.Label( "Class Loadout" );
+			}
+
+			public void Update( Class @class )
+			{
+				name.Text = @class.Name;
+				description.Text = @class.Description;
 			}
 		}
 	}
