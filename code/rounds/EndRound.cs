825af308ff7df00918340fe280161229807c4c21
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2022-02-11 12:58:30 -0400

    Impl EndRound, reset to 0 wins after

--- code/rounds/EndRound.cs
+++ code/rounds/EndRound.cs
@@ -2,10 +2,20 @@
 {
 	public partial class EndRound : BaseRound
 	{
+		public override string RoundName => "Game Over";
+		public override int RoundDuration => 15;
 
 		protected override void OnStart()
 		{
-			// Setup this round to celebrate whichever team won for x seconds, then transition into mapvote.
+			Log.Info( "Started End Round" );
+		}
+
+		protected override void OnTimeUp()
+		{
+			// Eventually, we will map vote so resetting this will be redundant.
+			Game.Instance.BlueWins = 0;
+			Game.Instance.RedWins = 0;
+			Game.Instance.ChangeRound( new BuildRound() );
 		}
 	}
 }
