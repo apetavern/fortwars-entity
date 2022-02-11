7b15f4aa5987f4bf7b5518ade1ef6a06cce37c7f
Brian Lindenhof <brian@shadowbrain.net>
2022-02-11 17:50:30 +0100

    make buildwheel disappear when holding Q and E

--- code/ui/hud/BuildWheel.cs
+++ code/ui/hud/BuildWheel.cs
@@ -12,13 +12,18 @@ namespace Fortwars
 			new ("1x4", "A tall panel good for ledges", "/ui/icons/1x4.png"),
 			new ("1x1x1", "A thicc block good for climbing", "/ui/icons/1x1x1.png"),
 			new ("1x2x1", "A thicc block good for cover", "/ui/icons/1x2x1.png"),
+			/*new ("metal3x2", "A wide panel good for defences", "/ui/models/blocks/fw_3x2.png"),
+			new ("metal1x2", "A medium panel good for entrances", "/ui/models/blocks/fw_1x2.png"),
+			new ("metal1x4", "A tall panel good for ledges", "/ui/models/blocks/fw_1x4.png"),
+			new ("metal1x1x1", "A thicc block good for climbing", "/ui/models/blocks/fw_1x1x1.png"),
+			new ("metal1x2x1", "A thicc block good for cover", "/ui/models/blocks/fw_1x2x1.png"),*/
 		};
 
 		public BuildWheel() : base()
 		{
 			VirtualCursor.OnClick += OnClick;
 
-			BindClass( "active", () => Input.Down( InputButton.Menu ) && Game.Instance.Round is BuildRound );
+			BindClass( "active", () => Input.Down( InputButton.Menu ) && !Input.Down( InputButton.Use ) && Game.Instance.Round is BuildRound );
 		}
 
 		private void OnClick()
