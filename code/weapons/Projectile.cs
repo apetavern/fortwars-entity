2f5052b6332b1d52259cb41e8f4a79f269fffdda
ImWill0w0 <28235017+ImWill0w0@users.noreply.github.com>
2022-02-15 11:17:43 -0800

    wip rpg smoke trail

--- code/weapons/Projectile.cs
+++ code/weapons/Projectile.cs
@@ -24,7 +24,7 @@ namespace Fortwars
 		{
 			if ( trailParticle == null )
 			{
-				trailParticle = Particles.Create( "particles/smoke_trail.vpcf" );
+				trailParticle = Particles.Create( "particles/rpg/rpg_trail_smoke.vpcf" );
 				trailParticle.SetPosition( 0, GetAttachment( "trail" ).Value.Position ); //Can't parent the particle or it gets destroyed with the projectile.
 			}
 
