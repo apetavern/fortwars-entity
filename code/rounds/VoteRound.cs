3a3d3d377d8bff3091f72037ee2e2a7ed5679552
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2022-02-17 16:01:48 -0400

    Start of mapvote, no client functionality yet

---
+++ code/rounds/VoteRound.cs
@@ -0,0 +1,44 @@
+﻿using Sandbox;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fortwars
+{
+	public partial class VoteRound : BaseRound
+	{
+		public override string RoundName => "Voting";
+		public override int RoundDuration => 300;
+
+		protected override void OnStart()
+		{
+			Log.Info( "Started Vote Round" );
+		}
+
+		protected override void OnTimeUp()
+		{
+			var game = Game.Instance;
+			if ( game == null ) return;
+
+			Dictionary<int, int> voteCount = new();
+			foreach ( var vote in game.MapVotes )
+			{
+				if ( !voteCount.ContainsKey( vote.MapIndex ) )
+					voteCount.Add( vote.MapIndex, 0 );
+
+				voteCount[vote.MapIndex]++;
+			}
+
+			var sortedMapVotePairs =
+				from entry in voteCount
+				orderby entry.Value descending
+				select entry;
+			if ( sortedMapVotePairs.Count() == 0 )
+			{
+				Global.ChangeLevel( Game.GetMaps()[0] );
+			}
+
+			var votedMap = sortedMapVotePairs.First();
+			Global.ChangeLevel( Game.GetMaps()[votedMap.Key] );
+		}
+	}
+}
