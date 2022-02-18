c7cfb009ae78c8bd92af4fa737997b3af86c8fde
Alex <alex@gu3.me>
2022-02-18 14:50:54 +0000

    Update build help buttons

--- code/ui/hud/BuildHelp.cs
+++ code/ui/hud/BuildHelp.cs
@@ -7,9 +7,8 @@ namespace Fortwars
 		public BuildHelp()
 		{
 			Add.InputHint( Sandbox.InputButton.Attack1, "Move" );
-			Add.InputHint( Sandbox.InputButton.Attack2, "Freeze" );
 			Add.InputHint( Sandbox.InputButton.Use, "Rotate" );
-			Add.InputHint( Sandbox.InputButton.Run, "Snap" );
+			Add.InputHint( Sandbox.InputButton.Run, "Snap Rotation" );
 			Add.InputHint( Sandbox.InputButton.Menu, "Build Menu" );
 
 			BindClass( "visible", () => Game.Instance.Round is BuildRound );
