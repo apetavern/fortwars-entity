86bff48f86767e8bee28afa6d987ca2a4eb8e2ce
Alex <alex@gu3.me>
2022-02-18 15:25:44 +0000

    Engineer should spend most of their time at home base - so give them AX50 instead of SMGII

--- code/player/classes/EngineerClass.cs
+++ code/player/classes/EngineerClass.cs
@@ -20,7 +20,7 @@ namespace Fortwars
 
 		public override List<string> CombatLoadout => new()
 		{
-			"fw:data/weapons/hksmgii.fwweapon",
+			"fw:data/weapons/aiax50.fwweapon",
 			"fw:data/weapons/trj.fwweapon",
 			"repairtool"
 		};
