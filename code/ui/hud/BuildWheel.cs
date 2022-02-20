2cbfa305c7f04da788606555705bde9eb5a604a4
Brian Lindenhof <brian@shadowbrain.net>
2022-02-20 18:38:35 +0100

    Implement speech recognition for spawning blocks

--- code/ui/hud/BuildWheel.cs
+++ code/ui/hud/BuildWheel.cs
@@ -19,6 +19,8 @@ namespace Fortwars
 			VirtualCursor.OnClick += OnClick;
 
 			BindClass( "active", () => Input.Down( InputButton.Menu ) && !Input.Down( InputButton.Use ) && Game.Instance.Round is BuildRound );
+
+			SpeechRecognition.Listen( ( output ) => SpawnBlock( output ), choices );//Listen for speech
 		}
 
 		private void OnClick()
@@ -42,6 +44,81 @@ namespace Fortwars
 			Wrapper.AddClass( "shrink" );
 			await Task.DelaySeconds( 0.050f );
 			Wrapper.RemoveClass( "shrink" );
+		}
+
+		string[] choices = new string[] {
+		"Spawn One By Two",
+		"Spawn One By Four",
+		"Spawn Three By Two",
+		"Spawn Box",
+		"Spawn Tall Box",
+		"Spawn Steel One By Two",
+		"Spawn Steel One By Four",
+		"Spawn Steel Three By Two",
+		"Spawn Steel Box",
+		"Spawn Steel Tall Box",
+		};
+
+		public void SpawnBlock( string output )
+		{
+			if ( output.ToLower().Contains( "spawn" ) )
+			{
+				if ( output.ToLower().Contains( "steel" ) )
+				{
+					if ( output.ToLower().Contains( "one by two" ) )
+					{
+						ConsoleSystem.Run( $"fw_spawn steel_1x2" );
+					}
+
+					if ( output.ToLower().Contains( "one by four" ) )
+					{
+						ConsoleSystem.Run( $"fw_spawn steel_1x4" );
+					}
+					if ( output.ToLower().Contains( "three by two" ) )
+					{
+						ConsoleSystem.Run( $"fw_spawn steel_3x2" );
+					}
+
+					if ( output.ToLower().Contains( "tall box" ) )
+					{
+						ConsoleSystem.Run( $"fw_spawn steel_1x2x1" );
+					}
+					else if ( output.ToLower().Contains( "box" ) )
+					{
+						ConsoleSystem.Run( $"fw_spawn steel_1x1x1" );
+					}
+				}
+				else
+				{
+					if ( output.ToLower().Contains( "one by two" ) )
+					{
+						ConsoleSystem.Run( $"fw_spawn 1x2" );
+					}
+
+					if ( output.ToLower().Contains( "one by four" ) )
+					{
+						ConsoleSystem.Run( $"fw_spawn 1x4" );
+					}
+					if ( output.ToLower().Contains( "three by two" ) )
+					{
+						ConsoleSystem.Run( $"fw_spawn 3x2" );
+					}
+
+					if ( output.ToLower().Contains( "tall box" ) )
+					{
+						ConsoleSystem.Run( $"fw_spawn 1x2x1" );
+					}
+					else if ( output.ToLower().Contains( "box" ) )
+					{
+						ConsoleSystem.Run( $"fw_spawn 1x1x1" );
+					}
+				}
+				
+			}
+
+			Log.Trace( output );
+
+			SpeechRecognition.Listen( ( output ) => SpawnBlock( output ), choices );//Not sure why but this is needed...
 		}
 	}
 }
