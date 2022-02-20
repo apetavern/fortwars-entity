e96d697df2e6e483868193e602b4d4e1bd5f4d41
Brian Lindenhof <brian@shadowbrain.net>
2022-02-20 23:48:47 +0100

    Try (and fail) to fix voice command spawning

--- code/components/SpeechRecognitionComponent.cs
+++ code/components/SpeechRecognitionComponent.cs
@@ -8,20 +8,40 @@ namespace Fortwars
 	{
 		public SpeechRecognitionComponent()
 		{
-			SpeechRecognition.Listen( ( output ) => SpawnBlock( output ), choices.Keys.ToArray() );//Listen for speech
+			SpeechRecognition.Listen( ( output ) => SpawnBlock( output ));//Listen for speech //, choices.Keys.ToArray() 
 		}
 
 		Dictionary<string, string> choices = new Dictionary<string, string>{
-			{ "Spawn One By Two","1x2" },
-			{ "Spawn One By Four","1x4" },
-			{ "Spawn Three By Two","3x2" },
-			{ "Spawn Box","1x1x1" },
-			{ "Spawn Tall Box","1x2x1" },
-			{ "Spawn Steel One By Two","steel_1x2" },
-			{ "Spawn Steel One By Four","steel_1x4" },
-			{ "Spawn Steel Three By Two","steel_3x2" },
-			{ "Spawn Steel Box","steel_1x1x1" },
-			{ "Spawn Steel Tall Box","steel_1x2x1" },
+			{ "Spawn door","1x2" },
+			{ "Spawn plank","1x4" },
+			{ "Spawn wall","3x2" },
+			{ "Spawn box","1x1x1" },
+			{ "Spawn tall box","1x2x1" },
+			{ "Spawn steel door","steel_1x2" },
+			{ "Spawn steel plank","steel_1x4" },
+			{ "Spawn steel wall","steel_3x2" },
+			{ "Spawn steel box","steel_1x1x1" },
+			{ "Spawn steel tall box","steel_1x2x1" },
+			{ "Spahn door","1x2" },
+			{ "Spahn plank","1x4" },
+			{ "Spahn wall","3x2" },
+			{ "Spahn box","1x1x1" },
+			{ "Spahn tall box","1x2x1" },
+			{ "Spahn steel door","steel_1x2" },
+			{ "Spahn steel plank","steel_1x4" },
+			{ "Spahn steel wall","steel_3x2" },
+			{ "Spahn steel box","steel_1x1x1" },
+			{ "Spahn steel tall box","steel_1x2x1" },
+			{ "Spawned door","1x2" },
+			{ "Spawned plank","1x4" },
+			{ "Spawned wall","3x2" },
+			{ "Spawned box","1x1x1" },
+			{ "Spawned tall box","1x2x1" },
+			{ "Spawned steel door","steel_1x2" },
+			{ "Spawned steel plank","steel_1x4" },
+			{ "Spawned steel wall","steel_3x2" },
+			{ "Spawned steel box","steel_1x1x1" },
+			{ "Spawned steel tall box","steel_1x2x1" },
 
 		};
 
@@ -32,7 +52,7 @@ namespace Fortwars
 
 			Log.Trace( output );
 
-			SpeechRecognition.Listen( ( output ) => SpawnBlock( output ), choices.Keys.ToArray() );//Not sure why but this is needed...
+			SpeechRecognition.Listen( ( output ) => SpawnBlock( output ));//Not sure why but this is needed...//, choices.Keys.ToArray() 
 		}
 	}
 }
