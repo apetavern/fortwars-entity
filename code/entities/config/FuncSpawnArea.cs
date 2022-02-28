85a71349f373fbfb6bc251c9f48be49af02e9a6c
Alex <alex@gu3.me>
2022-02-28 23:43:07 +0000

    Add `func_spawn_area` entity, kills players from other team

---
+++ code/entities/config/FuncSpawnArea.cs
@@ -0,0 +1,40 @@
+﻿using Sandbox;
+
+namespace Fortwars
+{
+	/// <summary>
+	/// Players capture the flag in this area.
+	/// </summary>
+	[Library( "func_spawn_area" )]
+	[Hammer.Solid]
+	[Hammer.RenderFields]
+	[Hammer.VisGroup( Hammer.VisGroup.Dynamic )]
+	public partial class FuncSpawnArea : BrushEntity
+	{
+		[Property]
+		public Team Team { get; set; }
+
+		public override void Spawn()
+		{
+			base.Spawn();
+
+			SetupPhysicsFromModel( PhysicsMotionType.Static );
+			CollisionGroup = CollisionGroup.Trigger;
+			EnableSolidCollisions = false;
+			EnableTouch = true;
+
+			Transmit = TransmitType.Never;
+		}
+
+		public override void StartTouch( Entity other )
+		{
+			base.StartTouch( other );
+
+			if ( other.IsWorld )
+				return;
+
+			if ( other is FortwarsPlayer player && player.TeamID != Team )
+				other.TakeDamage( DamageInfo.Generic( 10000f ) );
+		}
+	}
+}
