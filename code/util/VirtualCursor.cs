86875c85787fd36a6e0f0cb6946451d8f405c64a
Brian Lindenhof <brian@shadowbrain.net>
2022-03-06 12:14:42 +0100

    Fix virtual cursor to work with gamepads who use the track pad in mouse mode as their look mode

--- code/util/VirtualCursor.cs
+++ code/util/VirtualCursor.cs
@@ -25,9 +25,20 @@ namespace Fortwars
 		public static void Update()
 		{
 			if ( Input.UsingController )
-				Position = Input.GetAnalog( InputAnalog.Look ) * new Vector2( 1, -1 );
+			{
+				if (Input.GetAnalog(InputAnalog.Look) != Vector2.Zero) //Use joystick if it's been used at all
+				{
+					Position = Input.GetAnalog( InputAnalog.Look ) * new Vector2( 1, -1 );
+				}
+				else if( Mouse.Delta.Length > 50f ) //I use this instead of the position += method because trackpads feel a bit different than mice do, feels better to just set it to the delta position.
+				{
+					Position = Mouse.Delta * Time.Delta * 500; //Some controllers will still use the mouse. Need to make sure they can actually use the build menu...
+				}
+			}
 			else
+			{
 				Position += Mouse.Delta * Time.Delta * 500;
+			}
 		}
 
 		[Event.Frame]
