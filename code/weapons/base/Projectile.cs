bf1054564182474d2f6d5d8a6c5b890e90193a76
Alex <alex@gu3.me>
2022-04-12 17:11:26 +0100

    Refactor and move explosions into one function

--- code/weapons/base/Projectile.cs
+++ code/weapons/base/Projectile.cs
@@ -52,52 +52,7 @@ public class Projectile : ModelEntity
 
 	private void Explode( TraceResult tr )
 	{
-		if ( !IsServer )
-			return;
-
-		var sourcePos = tr.EndPosition;
-		var radius = 256f;
-		var overlaps = All.Where( e => Vector3.DistanceBetween( sourcePos, e.Position ) <= radius ).ToList();
-
-		foreach ( var overlap in overlaps )
-		{
-			if ( overlap is not ModelEntity ent || !ent.IsValid() ) continue;
-			if ( ent.LifeState != LifeState.Alive || !ent.PhysicsBody.IsValid() || ent.IsWorld ) continue;
-
-			var dir = ( overlap.Position - tr.EndPosition ).Normal;
-			var dist = Vector3.DistanceBetween( tr.EndPosition, overlap.Position );
-
-			if ( dist > radius ) continue;
-
-			var distanceFactor = 1.0f - Math.Clamp( dist / radius, 0, 1 );
-			var force = distanceFactor * ent.PhysicsBody.Mass;
-			force *= 0.5f; // Scale so it's not as strong
-
-			if ( ent.GroundEntity != null )
-			{
-				ent.GroundEntity = null;
-				if ( ent is Player { Controller: WalkController playerController } )
-					playerController.ClearGroundEntity();
-			}
-
-			bool shouldDoDamage = false;
-
-			if ( ent is not FortwarsPlayer player )
-				shouldDoDamage = true;
-			else if ( player.TeamID != ( Owner as FortwarsPlayer ).TeamID || Owner == player )
-				shouldDoDamage = true;
-
-			if ( shouldDoDamage )
-				ent.TakeDamage( DamageInfoExtension.FromProjectile( Weapon.WeaponAsset.MaxDamage * distanceFactor, Position, tr.Normal * 32, Client.Pawn ) );
-
-			ent.ApplyAbsoluteImpulse( dir * force );
-		}
-
-		using ( Prediction.Off() )
-		{
-			var particle = Particles.Create( "particles/explosion/fw_explosion_base.vpcf", Position );
-			PlaySound( "explosion.small" );
-		}
+		Game.Explode( tr.EndPosition, Owner, Weapon.WeaponAsset.MaxDamage );
 
 		Delete();
 	}
