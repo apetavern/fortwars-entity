9428174c0c3f225692e05b1cab7e3503d13a539d
Brian Lindenhof <brian@shadowbrain.net>
2022-09-05 01:03:55 +0200

    Update jumppadtool with new world model and overhead hold

--- code/weapons/tools/JumpPadTool.cs
+++ code/weapons/tools/JumpPadTool.cs
@@ -8,8 +8,36 @@ namespace Fortwars;
 [Library( "jumppadtool", Title = "Jump Pad" )]
 public partial class JumpPadTool : DropTool
 {
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		SetModel( "models/items/jumppad/jumppad.vmdl" );
+		TimeSinceLastDrop = DropTimeDelay;
+	}
+
 	public override void SpawnPickup()
 	{
 		ThrowProjectile( new JumpPad() );
+	}
+
+	public override void SimulateAnimator( PawnAnimator anim )
+	{
+		if ( TimeSinceLastDrop < DropTimeDelay )
+		{
+			EnableDrawing = false;
+			anim.SetAnimParameter( "holdtype", (int)HoldTypes.None );
+			anim.SetAnimParameter( "holdtype_handedness", (int)HoldHandedness.TwoHands );
+			anim.SetAnimParameter( "holdtype_pose_hand", 0f );
+			anim.SetAnimParameter( "holdtype_attack", 1 );
+		}
+		else
+		{
+			EnableDrawing = true;
+			anim.SetAnimParameter( "holdtype", (int)HoldTypes.HoldItem );
+			anim.SetAnimParameter( "holdtype_handedness", (int)HoldHandedness.OverHead );
+			anim.SetAnimParameter( "holdtype_pose_hand", 0f );
+			anim.SetAnimParameter( "holdtype_attack", 1 );
+		}
 	}
 }
