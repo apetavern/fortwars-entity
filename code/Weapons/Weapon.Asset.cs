873a35fa39e9cf01f2af8aa817b024ee2bce8d9d
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2023-01-17 00:14:49 -0400

    Begin work on Inventory system

---
+++ code/Weapons/Weapon.Asset.cs
@@ -0,0 +1,22 @@
+﻿namespace Fortwars;
+
+public partial class Weapon
+{
+	[Net]
+	private WeaponAsset Asset { get; set; }
+
+	public WeaponAsset WeaponAsset
+	{
+		get => Asset;
+		set
+		{
+			Asset = value;
+			SetupAsset( value );
+		}
+	}
+
+	protected void SetupAsset( WeaponAsset asset )
+	{
+		Model = asset.CachedWorldModel;
+	}
+}
--- code/player/Inventory.cs
+++ code/player/Inventory.cs
@@ -2,5 +2,35 @@
 
 public partial class Inventory : EntityComponent<Player>, ISingletonComponent
 {
+	[Net]
+	protected IList<Weapon> Weapons { get; set; }
+
+	[Net, Predicted]
+	public Weapon ActiveWeapon { get; set; }
+
+	[Net, Predicted]
+	public int LastActiveWeaponSlot { get; set; }
+
+	public int GetSlotFromWeapon( Weapon weapon ) => Weapons.IndexOf( weapon );
+	public int ActiveWeaponSlot => GetSlotFromWeapon( ActiveWeapon );
+
+
+	public void SetActiveWeapon ( Weapon weapon )
+	{
+		var currentWeapon = ActiveWeapon;
+		if ( currentWeapon.IsValid() )
+		{
+			currentWeapon.OnHolster( Entity );
+
+			if ( currentWeapon.IsValid )
+				LastActiveWeaponSlot = ActiveWeaponSlot;
+
+			ActiveWeapon = null;
+		}
+
+		ActiveWeapon = weapon;
+		weapon?.OnDeploy( Entity );
+	}
+
 	
 }
