ad0e0c1bc1b68b57277065de5c48d4ab48ccc020
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2023-01-19 19:10:34 -0400

    Shamelessly copy sbox-boomer's Gamemode system

---
+++ code/Gamemodes/Gamemode.cs
@@ -0,0 +1,22 @@
+﻿namespace Fortwars;
+
+public partial class Gamemode : Entity
+{
+	public virtual bool AllowMovement => true;
+	public virtual bool AllowDamage => true;
+	public virtual bool AllowFriendlyFire => false;
+
+	/// <summary>
+	/// Specify the list of teams that are supported in this mode.
+	/// </summary>
+	public virtual IEnumerable<Team> Teams => null;
+
+	public override void Spawn()
+	{
+		Transmit = TransmitType.Always;
+	}
+
+	internal virtual void Initialize() { }
+
+	internal virtual void OnPlayerKilled( Player player ) { }
+}
---
+++ code/Gamemodes/GamemodeSystem.cs
@@ -0,0 +1,54 @@
+﻿namespace Fortwars;
+
+public class GamemodeSystem
+{
+	[ConVar.Replicated]
+	public static string SelectedGamemode { get; set; } = "";
+
+	private static Gamemode _instance;
+
+	public static Gamemode Instance
+	{
+		get
+		{
+			if ( Game.IsServer )
+				return _instance;
+
+			if ( !_instance.IsValid() )
+				_instance = Entity.All.FirstOrDefault( x => x is Gamemode ) as Gamemode;
+
+			return _instance;
+		}
+		set
+		{
+			_instance = value;
+		}
+	}
+
+	protected static Gamemode FetchGamemodeEntity()
+	{
+		var gamemode = Entity.All.FirstOrDefault( x => x is Gamemode ) as Gamemode;
+
+		if ( !gamemode.IsValid() && !string.IsNullOrEmpty( SelectedGamemode ) )
+		{
+			var gamemodeEntity = TypeLibrary.Create<Gamemode>( SelectedGamemode );
+			if ( gamemodeEntity.IsValid() )
+			{
+				Log.Info( $"Fortwars: Found gamemode from Type - {SelectedGamemode}" );
+				return gamemodeEntity;
+			}
+
+			Log.Warning( "Fortwars: No gamemode found while fetching." );
+		}
+
+		return gamemode;
+	}
+
+	public static void SetupGamemode()
+	{
+		Instance = FetchGamemodeEntity();
+
+		if ( Instance.IsValid() )
+			Instance.Initialize();
+	}
+}
