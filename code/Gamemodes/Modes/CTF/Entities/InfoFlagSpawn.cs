6e3518d026d4b1b2e7748044a380b0cb9c46e25d
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2023-01-26 22:44:02 -0400

    Re-add InfoFlagSpawn, condense it and FlagDisplay

---
+++ code/Gamemodes/Modes/CTF/Entities/InfoFlagSpawn.cs
@@ -0,0 +1,41 @@
+﻿namespace Fortwars;
+
+[Library( "info_flag_spawn" )]
+[Title( "Flag Spawn" ), Category( "Fortwars" )]
+[EditorModel( "models/rust_props/small_junk/toilet_paper.vmdl" )]
+[HammerEntity]
+public partial class InfoFlagSpawn : ModelEntity
+{
+	[Net, Property, Change( nameof(OnTeamChanged) )]
+	public Team Team { get; set; }
+
+	private static readonly Model FlagModel = Model.Load( "models/items/bogroll/bogroll_w.vmdl" );
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		PhysicsEnabled = false;
+		UsePhysicsCollision = false;
+		EnableHideInFirstPerson = true;
+		EnableShadowInFirstPerson = true;
+
+		Model = FlagModel;
+	}
+
+	protected void OnTeamChanged()
+	{
+		switch ( Team )
+		{
+			case Team.Invalid: break;
+			case Team.Blue: 
+				SetMaterialGroup( 0 );
+				break;
+			case Team.Red:
+				SetMaterialGroup( 1 );
+				break;
+			default:
+				break;
+		}
+	}
+}
